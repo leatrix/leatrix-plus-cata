28bf113c7bdf2dbcae86bda4cd4233053fa6cb19
leatrix <support@leatrix.com>
2024-02-05 17:41:40 +0000

    3.0.178.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -4785,10 +4785,11 @@
 							_G[name]:GetScript("OnLeave")()
 							GameTooltip:Hide()
 						end)
-					elseif name == "BtWQuestsMinimapButton"
-						or name == "TomCats-MinimapButton"
-						or name == "LibDBIcon10_MethodRaidTools"
-						or name == "Lib_GPI_Minimap_LFGBulletinBoard"
+					elseif name == "BtWQuestsMinimapButton"				-- BtWQuests
+						or name == "TomCats-MinimapButton"				-- TomCat's Tours
+						or name == "LibDBIcon10_MethodRaidTools"		-- Method Raid Tools
+						or name == "Lib_GPI_Minimap_LFGBulletinBoard"	-- LFG Bulletin Board
+						or name == "wlMinimapButton"					-- Wowhead Looter (part of Wowhead client)
 						then
 						local myButton = LibStub("LibDBIcon-1.0"):GetMinimapButton("LeaPlusCustomIcon_" .. name)
 						myButton:HookScript("OnEnter", function()
